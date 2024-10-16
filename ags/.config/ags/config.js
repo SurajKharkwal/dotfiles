144ddb5c20b8e2c4dac65d72c38871c8221a17c6
SurajKharkwal <kharkwalsuraj13@gmail.com>
2024-10-17 00:09:55 +0530

    Initial Commit

---
+++ ags/.config/ags/config.js
@@ -0,0 +1,25 @@
+const time = Variable('', {
+  poll: [1000, function() {
+    return Date().toString()
+  }],
+})
+
+const Bar = (/** @type {number} */ monitor) => Widget.Window({
+  monitor,
+  name: `bar${monitor}`,
+  anchor: ['top', 'left', 'right'],
+  child: Widget.CenterBox({
+    start_widget: Widget.Label({
+      hpack: 'center',
+      label: 'Welcome to AGS!',
+    }),
+    end_widget: Widget.Label({
+      hpack: 'center',
+      label: time.bind(),
+    }),
+  }),
+})
+
+App.config({
+  windows: [Bar(0)],
+})
