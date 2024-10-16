dddaf0409a6a93ffe177e6fcfb3b10097052d6df
SurajKharkwal <kharkwalsuraj13@gmail.com>
2024-10-17 01:00:17 +0530

    Add: Hypr and the set wallpaper script

---
+++ hypr/.config/hypr/.config/hypr/scripts/set-wallpaper.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+wallpaper_root="/home/flying_hawk/Wallpapers" # Folder containing wallpapers
+
+# Find all wallpapers in the directory with specified file types
+wallpapers=($(find "$wallpaper_root" -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" -o -name "*.gif" -o -name "*.pnm" -o -name "*.tga" -o -name "*.tiff" -o -name "*.webp" -o -name "*.bmp" -o -name "*.farbfeld" \)))
+
+# Check if wallpapers were found
+if [ ${#wallpapers[@]} -eq 0 ]; then
+  echo "No wallpapers found."
+  exit 1
+fi
+
+# Extract just the filenames and add "Exit" option
+filenames=()
+for wallpaper in "${wallpapers[@]}"; do
+  filenames+=("$(basename "$wallpaper")")
+done
+filenames+=("Exit")
+
+# Display the wallpapers using wofi for selection
+selected=$(printf "%s\n" "${filenames[@]}" | wofi --dmenu --prompt "Select Wallpaper: ")
+
+# If the user selects "Exit", exit the script
+if [ "$selected" = "Exit" ]; then
+  exit
+fi
+
+# Find the full path of the selected wallpaper and set it using swww
+selected_path=$(find "$wallpaper_root" -type f -name "$selected")
+swww img "$selected_path"
