20808be3c66807667da59260956bd47197ad3454
SurajKharkwal <kharkwalsuraj13@gmail.com>
2024-12-23 15:43:34 +0000

    Code Refactor

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,17 +1,6 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "sql",
-  callback = function()
-    vim.opt_local.commentstring = "-- %s"
-  end,
-})
-
 vim.g.mapleader = " "
 vim.opt.relativenumber = true
-vim.g.prettier_cli_options = {
-  timeout = 5000, -- Timeout in milliseconds (e.g., 5 seconds)
-}
-
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
